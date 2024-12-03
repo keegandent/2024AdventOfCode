2273872cf8bdec18b9f648a7936bcdb66d013b97
Keegan Dent <keegandent@gmail.com>
2024-12-03 03:19:56 +0000

    brute force check working, bad and will change maybe

--- day02.py
+++ day02.py
@@ -7,10 +7,27 @@
 import sys
 
 import numpy as np
+from numpy.typing import ArrayLike
 
 ABS_DIFF_MAX = 3
 ABS_DIFF_MIN = 1
+NUM_SKIPS = 1
 
+
+def check(arr: ArrayLike, skips_left: int = NUM_SKIPS, dir=0) -> bool:
+    if skips_left < 0:
+        return False
+    diff = np.diff(arr)
+    dir = dir if dir != 0 else np.sign(diff[0])
+    for i, d in enumerate(diff):
+        d_abs = np.abs(d)
+        if np.sign(d) != dir or d_abs > ABS_DIFF_MAX or d_abs < ABS_DIFF_MIN:
+            return check(
+                np.concatenate((arr[:i], arr[i + 1 :])), skips_left - 1, dir
+            ) or check(
+                np.concatenate((arr[: i + 1], arr[i + 2 :])), skips_left - 1, dir
+            )
+    return True
 
 
 def main():
@@ -18,16 +35,16 @@ def main():
     for line in sys.stdin:
         line = line.rstrip()
         arr = np.fromstring(line, dtype=int, sep=" ")
-        diff = np.diff(arr)
-        if diff[0] == 0:
-            continue
-        diff_sign = np.sign(diff)
-        if np.any(diff_sign != diff_sign[0]):
-            continue
-        diff_abs = np.abs(diff)
-        if np.any(diff_abs > ABS_DIFF_MAX) or np.any(diff_abs < ABS_DIFF_MIN):
-            continue
-        count += 1
+        # count += check(arr)
+        # brute force check
+        bfc = check(arr, 0)
+        if bfc:
+            count += 1
+        else:
+            for i in range(arr.size):
+                if check(np.concatenate((arr[:i], arr[i + 1 :])), 0):
+                    count += 1
+                    break
     print(count)
 
 
