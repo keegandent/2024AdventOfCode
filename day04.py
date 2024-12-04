e1205177bd6c8afe322ba74a17faa75f7ad3fe8d
Keegan Dent <keegandent@gmail.com>
2024-12-04 06:44:26 +0000

    day 4 part 2 looks bad but done

--- day04.py
+++ day04.py
@@ -28,7 +28,7 @@ def grid_find(pattern: str, center: tuple, dir: tuple = None) -> int:
         return grid_find(
             pattern[1:], tuple([c + o for c, o in zip(center, dir, strict=True)]), dir
         )
-    for offset in product((-1, 0, 1), repeat=grid.ndim):
+    for offset in product((-1, 1), repeat=grid.ndim):
         coord = tuple([c + o for c, o in zip(center, offset, strict=True)])
         result += grid_find(pattern[1:], coord, offset)
     return result
@@ -41,7 +41,11 @@ def main():
     grid = np.loadtxt(f, dtype="U1", delimiter=",")
     count = 0
     for index, _ in np.ndenumerate(grid):
-        count += grid_find("XMAS", index)
+        if grid_find("AS", index) == 2 and grid_find("AM", index) == 2:
+            lt_corner = grid[tuple([i - 1 for i in index])]
+            rb_corner = grid[tuple([i + 1 for i in index])]
+            if lt_corner != rb_corner:
+                count += 1
     print(count)
 
 
