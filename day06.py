562476c73eba298e4e4a44ba9789850c175d65eb
Keegan Dent <keegandent@gmail.com>
2024-12-06 08:12:42 +0000

    day 6 part 2 sloooow

--- day06.py
+++ day06.py
@@ -8,6 +8,7 @@ import io
 import sys
 
 import numpy as np
+import tqdm
 
 global grid
 
@@ -17,10 +18,34 @@ RIGHT = np.array([[0, 1], [-1, 0]], dtype=int)
 def is_offgrid(position) -> bool:
     return any(
         [
-            (position[dim][0] < 0) or (position[dim][0] > grid.shape[dim])
+            (position[dim][0] < 0) or (position[dim][0] >= grid.shape[dim])
             for dim in range(grid.ndim)
         ]
     )
+
+
+def check_path(position, direction, new_obstructions=[]):
+    visited = set()
+    while True:
+        entry = tuple(
+            [int(pos[0]) for pos in position] + [int(d[0]) for d in direction]
+        )
+        if entry in visited:
+            return True
+        visited.add(entry)
+        while True:
+            new_position = position + direction
+            if is_offgrid(new_position):
+                return False
+            new_position_tup = tuple([int(pos[0]) for pos in new_position])
+            if (
+                new_position_tup not in new_obstructions
+                and grid[new_position_tup] != "#"
+            ):
+                break
+            direction = RIGHT @ direction
+            new_position = position + direction
+        position = new_position
 
 
 def main():
@@ -30,19 +55,13 @@ def main():
     grid = np.loadtxt(f, dtype="U1", comments=None, delimiter=",")
     direction = np.array([[-1], [0]], dtype=int)
     position = np.array(np.where(grid == "^"), dtype=int)
-    visited = set()
-    while True:
-        visited.add(tuple([int(pos[0]) for pos in position]))
-        while True:
-            new_position = position + direction
-            if is_offgrid(new_position):
-                print(len(visited))
-                return
-            if grid[tuple(new_position)] == ".":
-                break
-            direction = RIGHT @ direction
-            new_position = position + direction
-        position = new_position
+    s = 0
+    for coord in tqdm.tqdm(list(np.ndindex(grid.shape))):
+        if grid[coord] != ".":
+            continue
+        if check_path(position, direction, [coord]):
+            s += 1
+    print(s)
 
 
 if __name__ == "__main__":
