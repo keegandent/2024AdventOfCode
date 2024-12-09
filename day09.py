02e6836d324133f4c272f5cf8e61ee94cde74c2c
Keegan Dent <keegandent@gmail.com>
2024-12-09 00:37:27 -0600

    day 9 part 2 switched to a dict of indices and sizes

--- day09.py
+++ day09.py
@@ -9,20 +9,38 @@ import sys
 
 def main():
     inp = "".join([line.rstrip() for line in sys.stdin])
-    disk = []
+    disk_map = {None: []}
+    file_idx = 0
     # parse
     for i, char in enumerate(inp):
         file_id = i // 2 if i % 2 == 0 else None
-        disk.extend([file_id] * int(char))
+        space = int(char)
+        if file_id not in disk_map:
+            disk_map[file_id] = []
+        disk_map[file_id].append((file_idx, space))
+        file_idx += space
+    num_files = file_id + 1
     # compress
-    while True:
-        try:
-            new_idx = disk.index(None)
-        except ValueError:
-            break
-        disk[new_idx] = disk.pop()
+    for file_id in reversed(range(num_files)):
+        # real file ids have only one entry
+        orig_idx, space = disk_map[file_id][0]
+        for free_id, tup in enumerate(disk_map[None]):
+            file_idx, free_space = tup
+            if file_idx > orig_idx:
+                break
+            if free_space >= space:
+                disk_map[file_id][0] = (file_idx, space)
+                disk_map[None][free_id] = (file_idx + space, free_space - space)
+                if free_space == space:
+                    disk_map[None].pop(free_id)
+                disk_map[None].append((orig_idx, space))
+                break
     # checksum
-    print(sum([i * file_id for i, file_id in enumerate(disk)]))
+    s = 0
+    for file_id in range(num_files):
+        file_idx, space = disk_map[file_id][0]
+        s += sum([file_id * (file_idx + sub_idx) for sub_idx in range(space)])
+    print(s)
 
 
 if __name__ == "__main__":
