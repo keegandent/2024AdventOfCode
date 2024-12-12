3fd2dac71a8ed20966a27c2297289e3b55bb9f8b
Keegan Dent <keegandent@gmail.com>
2024-12-12 02:18:23 -0600

    still gross but marginally less

--- day12.py
+++ day12.py
@@ -34,16 +34,15 @@ def calculate_region_cost(region_type: str) -> int:
     type_spots = np.array(np.where(grid == region_type))
     regions = []
     for spot in type_spots.T:
-        sides = set()
+        sides = dict()
         candidates = set()
         for offset in np.vstack(
             [dir * np.eye(grid.ndim, dtype=int) for dir in [-1, 1]]
         ):
             position = spot + offset
             if is_offgrid(position) or grid[tuple(position)] != region_type:
-                sides.add(tuple(offset))
+                sides[tuple(offset)] = 1
 
-        orig_sides = sides.copy()
         for offset in np.vstack(
             [dir * np.eye(grid.ndim, dtype=int) for dir in [-1, 1]]
         ):
@@ -52,10 +51,12 @@ def calculate_region_cost(region_type: str) -> int:
                 dict_id = search_regions(regions, position)
                 candidates |= dict_id
                 if dict_id:
-                    sides -= regions[dict_id.pop()][tuple(position)][1]
+                    for side in regions[dict_id.pop()][tuple(position)]:
+                        if side in sides.keys():
+                            sides[side] = 0
 
         if not candidates:
-            regions.append({tuple(spot): (sides, orig_sides)})
+            regions.append({tuple(spot): sides})
         else:
             if len(candidates) > 1:
                 new_dict = {}
@@ -65,12 +66,12 @@ def calculate_region_cost(region_type: str) -> int:
                     regions.remove(od)
                 regions.append(new_dict)
                 candidates = set([len(regions) - 1])
-            regions[candidates.pop()][tuple(spot)] = (sides, orig_sides)
+            regions[candidates.pop()][tuple(spot)] = sides
 
     cost = 0
     for region in regions:
         area = len(region)
-        sides = sum([len(s) for s, _ in region.values()])
+        sides = sum([sum(s.values()) for s in region.values()])
         cost += area * sides
     return cost
 
