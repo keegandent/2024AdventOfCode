a1426355e231c0f030ece0a9512c603d2c1e8057
Keegan Dent <keegandent@gmail.com>
2024-12-14 04:03:48 -0600

    day 14 part 2 abusing day 12 code

--- day14.py
+++ day14.py
@@ -6,34 +6,89 @@
 
 import re
 import sys
-from math import prod
+from functools import cache
 
 import numpy as np
 
 GRID_SHAPE = np.array([101, 103])
-NUM_MOVES = 100
+BLOB_THRESHOLD = 4**2
+
+global grid
+grid = np.ndarray(GRID_SHAPE, dtype=int)
+
+
+@cache
+def offsets():
+    global grid
+    return np.vstack([dir * np.eye(grid.ndim, dtype=int) for dir in [-1, 1]])
+
+
+@cache
+def is_offgrid(position: tuple) -> bool:
+    global grid
+    return any(
+        [
+            (position[dim] < 0) or (position[dim] >= grid.shape[dim])
+            for dim in range(grid.ndim)
+        ]
+    )
+
+
+def largest_blob() -> int:
+    dots = np.array(np.where(grid > 0))
+    regions_lu = {}
+    num_regions = 0
+
+    for dot in dots.T:
+        candidate_regions = set()
+        for offset in offsets():
+            position = dot + offset
+            if (not is_offgrid(tuple(position))) and grid[tuple(position)] > 0:
+                try:
+                    candidate_regions.add(regions_lu[tuple(position)])
+                except KeyError:
+                    pass
+        if not candidate_regions:
+            candidate_regions.add(num_regions)
+            num_regions += 1
+        elif len(candidate_regions) > 1:
+            new_region = min(candidate_regions)
+            for k, v in regions_lu.items():
+                if v in candidate_regions:
+                    regions_lu[k] = new_region
+            candidate_regions = set([new_region])
+            # we do NOT update num_regions because we don't want to manage indices
+            # empty regions will be pruned at the end
+        regions_lu[tuple(dot)] = candidate_regions.pop()
+
+    regions = [0] * num_regions
+    for v in regions_lu.values():
+        regions[v] += 1
+    return max(regions)
 
 
 def main():
-    quads = [0, 0, 0, 0]
+    pos = []
+    vel = []
     for line in sys.stdin:
         m = re.match(r"^p=(\d+),(\d+)\s*v=(-?\d+),(-?\d+)$", line)
-        pos = np.array([int(m.group(1)), int(m.group(2))])
-        vel = np.array([int(m.group(3)), int(m.group(4))])
-        pos = (pos + NUM_MOVES * vel) % GRID_SHAPE
-        quad = [None, None]
-        if pos[0] < GRID_SHAPE[0] // 2:
-            quad[0] = 0
-        elif pos[0] >= GRID_SHAPE[0] - GRID_SHAPE[0] // 2:
-            quad[0] = 1
-        if pos[1] < GRID_SHAPE[1] // 2:
-            quad[1] = 0
-        elif pos[1] >= GRID_SHAPE[1] - GRID_SHAPE[1] // 2:
-            quad[1] = 2
-        if None in quad:
-            continue
-        quads[sum(quad)] += 1
-    print(prod(quads))
+        pos.append([int(m.group(1)), int(m.group(2))])
+        vel.append([int(m.group(3)), int(m.group(4))])
+    count = 0
+    while True:
+        grid[...] = 0
+        print(f"Count: {count:>6}")
+        for p, v in zip(pos, vel, strict=True):
+            p = (count * np.array(v) + np.array(p)) % GRID_SHAPE
+            grid[tuple(p)] += 1
+        blob = largest_blob()
+        print(f"Largest blob: {blob:>4}")
+        if blob > BLOB_THRESHOLD:
+
+            for row in grid:
+                print("".join(["#" if p else "." for p in row]))
+        print("", flush=True)
+        count += 1
 
 
 if __name__ == "__main__":
